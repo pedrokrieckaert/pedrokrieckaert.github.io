cc1602787a3d1c87d8a2f94316e1927554ce6d19
Pedro Krieckaert <58333196+PEEKrieckaert@users.noreply.github.com>
2020-02-10 12:42:37 +0100

    First Build

---
+++ precache-manifest.88ffd1da1ccedec4bd6cf1d80e959dbb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bf51ed774d71e8b1ceb1687c4b9b977",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2652687d3225ea00a29e",
+    "url": "/static/css/main.a809e46b.chunk.css"
+  },
+  {
+    "revision": "88c608eb3c375cb12b88",
+    "url": "/static/js/2.25432394.chunk.js"
+  },
+  {
+    "revision": "28ac78fef8675bfe412d94015e05d458",
+    "url": "/static/js/2.25432394.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2652687d3225ea00a29e",
+    "url": "/static/js/main.8a094d5b.chunk.js"
+  },
+  {
+    "revision": "37136e5acd31e79fc52a",
+    "url": "/static/js/runtime-main.0d6658c7.js"
+  }
+]);
